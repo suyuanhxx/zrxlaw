64868f85769d8cb291de337027e1c6697009e1f1
长大后能够飞 <huangxiaoxu93@gmail.com>
2016-11-11 17:54:19 +0800

    add ajxa request method

--- routes/login.js
+++ routes/login.js
@@ -3,18 +3,23 @@ var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  if('1'==req.params.id){
-    res.send('your input is 1, I am working well');
-  }
-  if('2'==req.params.id){
-    res.send('your input is 2, I am working well');
-  }
-  for(var i=3;i<100;i++){
-      if(i==req.params.id){
-         res.send('your input is ' + i + ', I am working well , node is so perfect');
-      }
-  } 
+/**
+ * var data = {
+                title:"title",
+                message:"message"
+            };
+ */
+router.post('/', function(req, res, next) {
+  var obj = req.body;
+	console.log('body: ' + JSON.stringify(req.body));
+	res.send(obj.title);
+});
+
+/**
+ * redirect 链接跳转
+ */
+router.get('/', function(req, res, next) {
+  res.redirect('../login.html');
 });
 
 module.exports = router;
