a86cdd75266e3cd70543a921f0d91fe84c674e9f
root <root@vultr.guest>
2017-01-06 09:46:25 +0000

    change config port

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.port = 3000;
 exports.email = 'huangxiaoxu93@gmail.com';
 exports.site_name = 'zrxlaw';
 // mongoDb connection
-// exports.db = 'mongodb://127.0.0.1:27017/scott_dev';
+exports.db = 'mongodb://127.0.0.1:27017/zrxlaw';
