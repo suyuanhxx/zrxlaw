0f54144a81593f6e067e591e98683960e604bc0a
xiaoxu.huang <xiaoxu.huang@baidao.com>
2017-02-08 17:58:49 +0800

    delete idea properties

---
+++ model/lawType.js
@@ -0,0 +1,26 @@
+/**
+ * Created by xiaoxu.huang on 2017/2/8.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var lawTypeSchema = new Schema({
+    name: String,
+    level: Number,//level=0表示最大
+    parentId: String,//所属父类id
+    description: String,
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+});
+
+
+mongoose.model('LawType', lawTypeSchema, 'lawType');
