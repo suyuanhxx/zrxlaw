e94e07f845f5e5fff5b4decd814176ea32b400e3
huangxiaoxu <huangxiaoxu93@gmail.com>
2017-02-27 21:19:24 +0800

    lawer query condition

---
+++ server/common/result.js
@@ -0,0 +1,12 @@
+/**
+ * Created by huangxiaoxu on 2017/2/27.
+ */
+'use strict';
+
+function Result() {
+    code:Number;
+    data:String;
+    msg:String;
+};
+var result = new Result();
+module.exports = result;
