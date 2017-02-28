17cfc58fdc130f8541a61b94c35c45e957442fb5
huangxiaoxu <huangxiaoxu93@gmail.com>
2017-02-28 23:03:48 +0800

    新增律师分页（瀑布流）查询

--- server/routes/lawer.js
+++ server/routes/lawer.js
@@ -1,11 +1,23 @@
 var Lawer = require('../controller/lawer');
 
 module.exports = function (app) {
-    // 获取所有律師
-    app.get('/lawers', Lawer.getLawers);
+    /**
+     * 瀑布流获取律师信息（名称，职位，描述，头像）
+     */
+    app.get('/lawers/:page/:size', Lawer.getLawersByWaterFlow);
+
+    /**
+     * 获取首页展示律师信息（名称，职位，描述，头像）
+     */
     app.get('/condition/lawers', Lawer.getTopLawers);
-    // 根据关键字搜索律师
+
+    /**
+     * 根据关键字搜索律师
+     */
     app.get('/lawer/:keyword', Lawer.getLawer);
-    //添加一个律師
+
+    /**
+     * 添加律师
+     */
     app.post('/lawer', Lawer.addLawer);
 };
