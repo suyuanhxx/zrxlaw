71da480b8ed4b18b4e5a7d11889bb245427e7607
xiaoxu.huang <xiaoxu.huang@baidao.com>
2017-03-01 16:10:17 +0800

    vue test

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('./common/logger');
@@ -12,10 +10,14 @@ var app = express();
 
 // view engine setup
 // # npm install consolidate mustache --save
+var express = require('express');
+var cons = require('consolidate');
+var app = express();
+
 var engines = require('consolidate');
-app.set('views', path.join(__dirname, '../client/views'));
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../client/views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
