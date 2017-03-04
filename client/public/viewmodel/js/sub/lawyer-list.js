6ae4dcc3602479e1490ec4db0d653e56d9362898
yichenw7 <yichenw7@126.com>
2017-03-04 16:06:02 +0800

    sub-lawyer-瀑布流1.0

---
+++ client/public/viewmodel/js/sub/lawyer-list.js
@@ -0,0 +1,42 @@
+/**
+ * Created by yichenw7 on 2017/3/2.
+ */
+$(function () {
+  //
+  // lawyerlist-waterfall
+  //
+
+
+  lawyerlist.waterfallset();
+
+});
+var lawyerlist={
+waterfallset:function () {
+
+
+  $('#container-wrapper').waterfall({
+    itemCls: 'item',
+    colWidth: 222,
+    gutterWidth: 15,
+    gutterHeight: 15,
+    checkImagesLoaded: false,
+    path: function(page) {
+      return '/lawers/'+ page;
+    },
+    callbacks: {
+      /*
+       * 处理ajax返回数方法
+       * @param {String} data
+       */
+      renderData: function (data) {
+        var template = $('#waterfall-tpl').html();
+
+        return Mustache.to_html(template, data).replace(/^\s*/mg, '')
+      }
+    }
+  });
+
+}
+
+
+}
---
+++ client/public/viewmodel/src/page/sub/lawyer/list/lawyer-list.vue
@@ -0,0 +1,20 @@
+<template id="lawyer-template">
+
+    <div id="lawyer-list">
+        {{title}}
+
+    </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+  Vue.component('app-header', {
+    template: '#lawyer-list',
+    data: {
+      title: '我是頭部'
+    }
+  })
+
+</script>
+    
