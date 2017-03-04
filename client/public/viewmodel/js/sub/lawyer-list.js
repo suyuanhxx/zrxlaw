2b69cb7128372fcc944fdb66a2405c0eaf102d2f
yichenw7 <yichenw7@126.com>
2017-03-04 16:37:13 +0800

    sub-lawyer-瀑布流1.0

--- client/public/viewmodel/js/sub/lawyer-list.js
+++ client/public/viewmodel/js/sub/lawyer-list.js
@@ -10,33 +10,33 @@ $(function () {
   lawyerlist.waterfallset();
 
 });
-var lawyerlist={
-waterfallset:function () {
+var lawyerlist = {
+  waterfallset: function () {
 
 
-  $('#container-wrapper').waterfall({
-    itemCls: 'item',
-    colWidth: 222,
-    gutterWidth: 15,
-    gutterHeight: 15,
-    checkImagesLoaded: false,
-    path: function(page) {
-      return '/lawers/'+ page;
-    },
-    callbacks: {
-      /*
-       * 处理ajax返回数方法
-       * @param {String} data
-       */
-      renderData: function (data) {
-        var template = $('#waterfall-tpl').html();
+    $('#container-wrapper').waterfall({
+      itemCls: 'item',
+      colWidth: 222,
+      gutterWidth: 15,
+      gutterHeight: 15,
+      checkImagesLoaded: false,
+      path: function (page) {
+        return '/lawyers/' + page + '/10';
+      },
+      callbacks: {
+        /*
+         * 处理ajax返回数方法
+         * @param {String} data
+         */
+        renderData: function (data) {
+          var template = $('#waterfall-tpl').html();
 
-        return Mustache.to_html(template, data).replace(/^\s*/mg, '')
+          return Mustache.to_html(template, data).replace(/^\s*/mg, '')
+        }
       }
-    }
-  });
+    });
 
-}
+  }
 
 
 }
