7a65e176658aa00ebfa4164fda382c5b72e5420f
xiaoxu.huang <xiaoxu.huang@baidao.com>
2017-03-10 15:49:10 +0800

    阿里OSS

--- server/common/fileHelper.js
+++ server/common/fileHelper.js
@@ -3,15 +3,34 @@
  */
 var fs = require('fs');
 var logger = require('../common/logger');
-var crypto = require('crypto');
+var oss = require('ali-oss');
+var co = require('co');
+var client = oss({
+    accessKeyId: 'LTAIuBSH1GS9KA6Q',
+    accessKeySecret: 'JS9cFycoEzx4VTMv6OpDpGvY7zhxJ3',
+    bucket: 'zrxlaw',
+    region: 'oss-cn-shanghai'
+});
 /**
  * 文件上传功能
  */
 exports.fileUpLoad = function (file, callback) {
-    fs.readFile('server/logs/zrxlaw.log', function (err, data) {
-        if (err) {
-            logger.error(err);
-        }
-        callback(err, data);
+    // var hash = crypto.createHash('md5');
+    // fs.readFile('server/logs/zrxlaw.log', function (err, data) {
+    //     if (err) {
+    //         logger.error(err);
+    //     }
+    //     hash.update(data);
+    //     callback(err, data);
+    // });
+    var filepath = 'server/logs/zrxlaw.log';
+    // var object = yield store.put('ossdemo/demo.txt', filepath);
+    // console.log(object);
+
+    co(function* () {
+        yield client.put('ossdemo/zrxlaw.log', filepath);
+
+    }).catch(function (err) {
+        console.log(err);
     });
 };
